701ab862d8a3c047b60554c24cd7f8dfd1567824
galvarezm86 <101333812+galvarezm86@users.noreply.github.com>
2026-07-08 15:20:46 +0000

    Feat: Agrega endpoint /health para automatizaciones

--- flask-app/app.py
+++ flask-app/app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, request, redirect, url_for, session, flash, abort, send_file
+from flask import Flask, render_template, request, redirect, url_for, session, flash, abort, send_file, Response
 import os
 import secrets
 import unicodedata
@@ -141,6 +141,10 @@ logging.info(
 def inicio():
     return render_template("inicio.html")
 
+@app.get("/health")
+def health():
+    return Response("OK", status=200, mimetype="text/plain")
+    
 @app.route("/login", methods=["GET", "POST"])
 def login():
 
